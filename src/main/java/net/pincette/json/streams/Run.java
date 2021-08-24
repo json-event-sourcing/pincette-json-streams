13f865ad67bea8c1fbfcd612ddd4e5440a605c9c
Werner Donné <werner.donne@pincette.biz>
2021-08-24 10:59:17 +0200

    Update dependencies

--- src/main/java/net/pincette/json/streams/Run.java
+++ src/main/java/net/pincette/json/streams/Run.java
@@ -169,7 +169,7 @@ import picocli.CommandLine.Option;
     description = "Runs topologies from a file containing a JSON array or a MongoDB collection.")
 class Run implements Runnable {
   private static final String APPLICATION_ID = "application.id";
-  private static final String APP_VERSION = "1.7.11";
+  private static final String APP_VERSION = "1.7.12";
   private static final String CONTEXT_PATH = "contextPath";
   private static final Duration DEFAULT_RESTART_BACKOFF = ofSeconds(10);
   private static final String EVENT = "$$event";
